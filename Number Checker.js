2a75bdda6bae3a85fa39cc6e7d5b9884f41a8ea0
RahulKhartude76 <khartuderahul0@gmail.com>
2024-10-07 11:14:59 +0530

    html,js

---
+++ Number Checker.js
@@ -0,0 +1,21 @@
+// Positive or Negative Number Checker 
+
+function numberChecking(number){
+    if(number > 0){
+      console.log("The number is positive");
+    }
+    else if(number < 0){
+      console.log("The number is negative");
+    }
+    else{
+      console.log("The number is zero");
+    }
+  }
+  numberChecking(-15);
+  numberChecking(0);
+  numberChecking(1);
+
+  let n = 6;
+for (let i = 1; i <= 10; ++i)
+    console.log(n + " * " + i +
+        " = " + n * i);
